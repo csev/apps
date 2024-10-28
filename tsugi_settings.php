fa8eef5e2158ec480fe203dd66b3f210f688d8da
Charles Severance <csev@umich.edu>
2024-10-28 10:20:23 -0400

    Updates.

---
+++ tsugi_settings.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * These are some configuration variables that are not secure / sensitive
+ *
+ * This file is included at the end of tsugi/config.php
+ */
+
+// This is how the system will refer to itself.
+$CFG->servicename = "Tsugi@UMSI";
+$CFG->servicedesc = 'This learning app store is supported by Charles Severance and contains a number of applications that can be used in Canvas, Coursera, or any other Learning sysem.';
+
+// Default theme
+
+$CFG->context_title = "UMich Learning Applications";
+
